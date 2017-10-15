18a76faf9a9528296f52fd3a78329e7b7ffc53be
Lming1 <spear131121@gmail.com>
2017-10-15 20:34:15 +0900

    camera fix

---
+++ app/src/main/java/com/example/iminhyeok/travewel/CameraActivity.java
@@ -0,0 +1,10 @@
+package com.example.iminhyeok.travewel;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by iminhyeok on 2017. 10. 14..
+ */
+
+public class CameraActivity extends AppCompatActivity {
+}
